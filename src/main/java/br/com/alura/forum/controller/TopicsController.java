aaa3b3fd5aa975f46420efafef7d443eec9c4178
Victor Vieira <victor.h.souza.vieira@gmail.com>
2021-12-23 16:50:30 -0300

    feat: Changes Controller by RestController

--- src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -2,19 +2,16 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.Topic;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Arrays;
 
-@Controller
+@RestController
 public class TopicsController {
 
     @RequestMapping("/topics")
-    @ResponseBody
     public List<Topic> list() {
         Topic topic = new Topic(
                 "Dúvida",
